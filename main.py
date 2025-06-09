84e5926ea8d2276a2fe50870e2ed261b892b2c09
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-06-09 22:00:37 +0600

    update main.py to adjust window size and add right-side text box for file input

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import scrolledtext
 
 print("Text Diff Viewer Starting...")
 root = tk.Tk()
-root.geometry("900x1200")
+root.geometry("1200x1200")
 root.title("Visual Text Diff Viewer")
 
 # Label on top
@@ -14,5 +14,11 @@ tk.Label(root, text="Left File").grid(row=0, column=0, padx=10, pady=5)
 left_text = scrolledtext.ScrolledText(root, width=60, height=20)
 left_text.grid(row=1, column=0, padx=10, pady=5)
 
+# Right label
+tk.Label(root, text="Right File").grid(row=0, column=1, padx=10, pady=5)
+
+# Right-side text box
+right_text = scrolledtext.ScrolledText(root, width=60, height=20)
+right_text.grid(row=1, column=1, padx=10, pady=5)
 
 root.mainloop()
