f93f2de90f3ffe557ee5988817d89222a710daf5
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-06-09 21:59:29 +0600

    add left-side text box to main.py for file input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import scrolledtext
 
 
 print("Text Diff Viewer Starting...")
@@ -9,6 +10,9 @@ root.title("Visual Text Diff Viewer")
 # Label on top
 tk.Label(root, text="Left File").grid(row=0, column=0, padx=10, pady=5)
 
+# Left-side text box
+left_text = scrolledtext.ScrolledText(root, width=60, height=20)
+left_text.grid(row=1, column=0, padx=10, pady=5)
 
 
 root.mainloop()
